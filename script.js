839ac394d758f95febe8a0cd9142e3c7db4b8bf2
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-05-23 14:00:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	const table = document.getElementById('sampleTable');
+  const newRow = table.insertRow(0); // Insert at position 1 (just after the header row)
+  newRow.insertAdjacentHTML('beforeend', '<td>New Cell1</td><td>New Cell2</td>');
 }
